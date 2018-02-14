9cb75172575e30add2a0716928d387576e2f2868
ac065455 <chen.anthony.1109@gmail.com>
2018-02-14 10:03:37 -0500

    added online order

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,12 @@ import Logo from '../static/images/logo.png';
 
 const Header = () =>
   <div className="header">
-    <div className="header-logo"><img src={Logo} alt="logo"/></div>
+    <div className="header-logo">
+      <img src={Logo} alt="logo"/>
+      <div className="order-btn">
+        <p className="glf-button" data-glf-cuid="5aac8e66-7ca3-4cde-903b-c1cc04f35b48" data-glf-ruid="95a517d9-d1ef-4165-a74e-cfcd76b2bd07"> See MENU & Order</p>
+      </div>
+    </div>
   </div>
 
 export default Header;
