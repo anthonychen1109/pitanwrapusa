0a58cf7585bc8d3c2787ef471909e2cddad4b2dc
ac065455 <chen.anthony.1109@gmail.com>
2018-03-05 16:19:27 -0500

    removed # from all anchors in navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import $ from 'jquery';
 
 class Navbar extends Component {
   constructor(props) {
@@ -18,15 +19,19 @@ class Navbar extends Component {
     }
   }
 
+  goToByScroll(id){
+    $('html,body').animate({scrollTop: $("#"+id).offset().top},'fast');
+  }
+
   render() {
     return (
       <nav>
         <ul className="topnav" id="dropdownClick">
           <li className="logo"><a href="/">Pita & Wrap</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#menu">Menu</a></li>
+          <li><span onClick={() => this.goToByScroll('about')}>About</span></li>
+          <li><span onClick={() => this.goToByScroll('menu')}>Menu</span></li>
           <li><span data-glf-cuid="5aac8e66-7ca3-4cde-903b-c1cc04f35b48" data-glf-ruid="95a517d9-d1ef-4165-a74e-cfcd76b2bd07">Order Online</span></li>
-          <li><a href="#contact">Contact</a></li>
+          <li><span onClick={() => this.goToByScroll('contact')}>Contact</span></li>
           <li id="test" className="dropdownIcon">
             <a href="javascript:void(0);"
               onClick={this.dropdownMenu}
