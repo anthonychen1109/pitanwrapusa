b31d5b60dce9c68f3d9ff667a4a29d0c2635a144
ac065455 <chen.anthony.1109@gmail.com>
2018-03-05 16:02:00 -0500

    removed # when clicking order online

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -25,7 +25,7 @@ class Navbar extends Component {
           <li className="logo"><a href="/">Pita & Wrap</a></li>
           <li><a href="#about">About</a></li>
           <li><a href="#menu">Menu</a></li>
-          <li><a data-glf-cuid="5aac8e66-7ca3-4cde-903b-c1cc04f35b48" data-glf-ruid="95a517d9-d1ef-4165-a74e-cfcd76b2bd07" href="#">Order Online</a></li>
+          <li><span data-glf-cuid="5aac8e66-7ca3-4cde-903b-c1cc04f35b48" data-glf-ruid="95a517d9-d1ef-4165-a74e-cfcd76b2bd07">Order Online</span></li>
           <li><a href="#contact">Contact</a></li>
           <li id="test" className="dropdownIcon">
             <a href="javascript:void(0);"
